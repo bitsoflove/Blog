399934e7846203bf49977da2a99031c2fc68c6ab
Stijn Tilleman <stijn@bitsoflove.be>
2016-05-30 13:36:55 +0200

    PSR2

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -2,16 +2,15 @@
 
 use Illuminate\Routing\Router;
 
-/** @var Router $router */
+/* @var Router $router */
 
 $router->group(['prefix' => 'blog'], function (Router $router) {
     $locale = LaravelLocalization::setLocale() ?: App::getLocale();
-    $router->get('posts', ['as' => $locale . '.blog', 'uses' => 'PublicController@index']);
-    $router->get('posts/{slug}', ['as' => $locale . '.blog.slug', 'uses' => 'PublicController@show']);
+    $router->get('posts', ['as' => $locale.'.blog', 'uses' => 'PublicController@index']);
+    $router->get('posts/{slug}', ['as' => $locale.'.blog.slug', 'uses' => 'PublicController@show']);
 });
 
-
-$router->get('modules/blog/{folder}/{filename}', function($folder, $filename) {
-    $content = file_get_contents(__DIR__ . '/../Assets/' . $folder . '/' . $filename);
+$router->get('modules/blog/{folder}/{filename}', function ($folder, $filename) {
+    $content = file_get_contents(__DIR__.'/../Assets/'.$folder.'/'.$filename);
     die($content);
 });
