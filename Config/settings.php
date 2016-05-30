ab87a7d76a17220ce4f83305d8caeb746d3a7b30
Stijn Tilleman <stijn@bitsoflove.be>
2016-05-30 13:36:27 +0200

    PSR2

--- Config/settings.php
+++ Config/settings.php
@@ -2,13 +2,13 @@
 
 return [
     'posts-per-page' => [
-        'description'  => 'blog::settings.posts-per-page',
-        'view'         => 'text',
+        'description' => 'blog::settings.posts-per-page',
+        'view' => 'text',
         'translatable' => true,
     ],
-    'latest-posts-amount'  => [
-        'description'  => 'blog::settings.latest-posts-amount',
-        'view'         => 'text',
+    'latest-posts-amount' => [
+        'description' => 'blog::settings.latest-posts-amount',
+        'view' => 'text',
         'translatable' => true,
     ],
 ];
